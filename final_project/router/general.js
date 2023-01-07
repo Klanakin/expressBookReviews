e3fac570f8672b74e00328b286f272a0e78a1d64
Don Klanakin <don.klanakin@hotmail.com>
2023-01-07 08:29:42 -0500

    Modify general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,41 +6,42 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.send(JSON.stringify(books));
+    //Write your code here
+    return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
-  const bookEntries = Object.entries(books);
-  const bookByISBN = bookEntries.filter((book) => book[isbn] === isbn);
-  return res.send(JSON.stringify(bookByISBN));
+    //Write your code here
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const author = req.params.author;
+    const bookEntries = Object.entries(books);
+    const booksByAuthor = bookEntries.filter(([key, value]) => value.author === author);
+    return res.send(booksByAuthor);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
